9573da4316061a79cb1c369e90ecbbc14d6598b9
peter115342 <pmuzslay8@gmail.com>
2023-10-22 15:30:12 +0200

    submit button function check

--- hlkannakjbnaajgajf/script.js
+++ hlkannakjbnaajgajf/script.js
@@ -11,6 +11,7 @@ function fillEmail() {
 function showError(message,errId) {
     let err = document.getElementById(errId);
     err.style.visibility = "visible";
+    
     err.innerHTML = message;
 }
 
@@ -22,11 +23,54 @@ function hideError(errId) {
 
 function checkEmpty(field, errId) {
     let inputField = document.getElementById(field);
-    let errorField = document.getElementById(errId);
 
-    if (!inputField.value) {
+    if (!inputField.value.trim()) {
         showError("This is a required field!", errId);
+        inputField.style.borderColor = "rgb(179, 6, 6)";
+        inputField.style.borderWidth = "4px";
+        return 1;
+
     } else {
         hideError(errId);
+        inputField.style.borderColor = "rgb(0, 70, 68)";
+        inputField.style.borderWidth = "3px";
+        return 0;
+
     }
 }
+    function ageAnnotationShow(errId) {
+        showError("Age is determined automatically.", errId);
+    }
+    function ageAnnotationHide(errId) {
+        hideError(errId);
+    }
+
+
+    function checkRequiredFields(formId, buttonId) {
+        const form = document.getElementById(formId);
+        const requiredFields = form.querySelectorAll('[required]');
+        for (const field of requiredFields) {
+            if (!field.value.trim()) {
+                hideSubmitButton(buttonId);
+                console.log("0");
+                return 0;
+            }
+        }
+        showSubmitButton(buttonId);
+        console.log("1");
+        return 1;
+
+    }
+
+
+    function showSubmitButton(buttonId){
+        let button = document.getElementById(buttonId)
+        button.visibility = "visible";
+    }
+    
+    function hideSubmitButton(buttonId){
+        let button = document.getElementById(buttonId)
+        button.visibility = "hidden";
+    }
+
+
